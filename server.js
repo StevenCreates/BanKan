4504220845a00e8e5f5bbe6b1cb6fa7a681a16f6
Steven Hofheins <stevenisinsane@gmail.com>
2020-02-08 20:35:04 -0700

    breaking things until heroku works

--- server.js
+++ server.js
@@ -1,48 +1,23 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const passport = require("passport");
-const users = require("./routes/api/users");
-const posts = require("./routes/api/posts");
-const profile = require("./routes/api/profile");
-const comments = require("./routes/api/comments");
-const app = express();
 const path = require("path");
-const dotenv = require("dotenv");
-dotenv.config();
-
-app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
-);
 
 const PORT = process.env.PORT || 3001;
+const app = express();
+const apiRoutes = require("./routes/apiRoutes");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
-// app.use(bodyParser.json());
-// DB Config
-const db = require("./config/keys").mongoURI;
-// Connect to MongoDB
-mongoose
-  .connect(db, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true
-  })
-  .then(() => console.log("MongoDB successfully connected"))
-  .catch(err => console.log(err));
+// Use apiRoutes
+app.use("/api", apiRoutes);
 
-// Passport middleware
-app.use(passport.initialize()); // Passport config
-require("./config/passport")(passport); // Routes
-app.use("/api/users", users);
-app.use("/api/posts", posts);
-app.use("/api/profile", profile);
-app.use("/api/comments", comments);
-
+// Send every request to the React app
+// Define any API routes before this runs
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
